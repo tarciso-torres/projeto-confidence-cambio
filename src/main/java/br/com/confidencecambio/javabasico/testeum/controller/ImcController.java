4c6d188b7dac9e3a1cb26b86781ac8708afd9d4e
Tarciso Torres <tarcisoftorres@gmail.com>
2020-12-10 15:15:19 -0300

    feat: criacao do ImcController

---
+++ src/main/java/br/com/confidencecambio/javabasico/testeum/controller/ImcController.java
@@ -0,0 +1,31 @@
+package br.com.confidencecambio.javabasico.testeum.controller;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Positive;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.confidencecambio.javabasico.testeum.customexceptions.InternalServerErrorException;
+import br.com.confidencecambio.javabasico.testeum.service.ImcService;
+
+@RestController("/imc")
+@Validated
+public class ImcController {
+	
+	@Autowired
+	private ImcService service;
+	
+	@GetMapping()
+	public ResponseEntity<Double> calculaImc(@RequestParam @NotEmpty @Positive double altura, 
+											 @RequestParam @NotEmpty @Positive double peso) throws InternalServerErrorException{
+		
+		return new ResponseEntity<Double>(service.calculaImc(altura, peso), HttpStatus.OK);
+	}
+
+}
